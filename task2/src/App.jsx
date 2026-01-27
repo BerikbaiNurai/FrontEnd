e1b7d276c90d0faeaabbe36f1f9085bbcdf291dc
Nurai Berikbai <nurajberikbaj@gmail.com>
2026-01-27 19:37:30 +0500

    second commit

---
+++ task2/src/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div>
+      <h1 style = {{textAlign: "center"}}>Lab1</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
